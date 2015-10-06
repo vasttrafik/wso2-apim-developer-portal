9eb9b4e9ff538ede95c07a5fb3228d54423c0af0
Mikael Lindberg <mikael@lindbergit.se>
2015-10-06 11:48:59 +0200

    Directive and index update for footer

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 app = angular.module('vtPortal', [])
-.directive('header', function(){
+    .directive('header', function(){
       return {
         templateUrl: 'partials/header.html'
       }
-    });
+    })
+.directive('footer', function () {
+    return {
+        templateUrl: 'partials/footer.html'
+    }
+})
 
 app.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
 
