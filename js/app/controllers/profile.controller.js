7ca1fdd87357a39e38577784470bb4bab50cfd9c
Christian Morin <christian.morin@vasttrafik.se>
2015-10-19 17:18:41 +0200

    Added handling for profile view.

--- js/app/controllers/profile.controller.js
+++ js/app/controllers/profile.controller.js
@@ -5,22 +5,100 @@
     .module('vtPortal')
     .controller('ProfileCtrl', ProfileCtrl);
 
-  ProfileCtrl.$inject = ['$http', '$httpParamSerializer', 'APIService', 'AlertService'];
+  ProfileCtrl.$inject = ['$rootScope', '$timeout', '$location', '$http', '$httpParamSerializer', 'APIService', 'AlertService', 'UserService'];
 
-  function ProfileCtrl($http, $httpParamSerializer, APIService, AlertService) {
+  function ProfileCtrl($rootScope, $timeout, $location, $http, $httpParamSerializer, APIService, AlertService, UserService) {
     var vm = this;
 
-    APIService.call('applicationsGet', [0.0, 0.0])
-      .then(ApplicationsGetResponse);
+    vm.saveProfile = saveProfile;
+    vm.savePassword = savePassword;
+    vm.resetProfileForm = resetProfileForm;
+    vm.resetPasswordForm = resetPasswordForm;
 
-    function ApplicationsGetResponse(response) {
-      if (response.status === 200) {
-        vm.applications = response.data.list;
-      } else {
-        AlertService.error("Problem retrieving application list");
+    vm.user = angular.copy($rootScope.globals.currentUser);
+
+    // angular.element(document).ready(function() {
+    //   $timeout(function() {
+    //     angular.element('#editButton').trigger('click');
+    //   }, 100);
+    // });
+
+
+    function saveProfile() {
+
+      vm.dataLoadingProfile = true;
+
+      UserService.getUser()
+        .then(function(response) {
+
+          response.userName = vm.user.userName;
+          response.claims = [{
+            claimURI: 'http://wso2.org/claims/emailaddress',
+            value: vm.user.email
+          }, {
+            claimURI: 'http://wso2.org/claims/givenname',
+            value: vm.user.firstName
+          }, {
+            claimURI: 'http://wso2.org/claims/lastname',
+            value: vm.user.lastName
+          }];
+
+          APIService.call('usersUserIdPut', ['updateProfile', response, response.userId])
+            .then(usersUserIdPutResponse);
+        });
+
+      function usersUserIdPutResponse(response) {
+
+        $timeout(function() {
+          UserService.getUser()
+            .then(function(userResponse) {
+              userResponse.userName = response.data.userName;
+              userResponse.claims = response.data.claims;
+              UserService.setUser(userResponse)
+                .then(function() {
+                  vm.user = angular.copy($rootScope.globals.currentUser);
+                  vm.dataLoadingProfile = false;
+                  AlertService.success("Din profil är uppdaterad!");
+                });
+            });
+
+        }, 1000);
       }
+
+    }
+
+    function savePassword() {
+
+      vm.dataLoadingPassword = true;
+
+      UserService.getUser()
+        .then(function(response) {
+
+          APIService.call('usersUserIdPut', ['updateCredential', response, response.userId])
+            .then(usersUserIdPutResponse);
+        });
+
+      function usersUserIdPutResponse(response) {
+
+        $timeout(function() {
+          vm.dataLoadingPassword = false;
+          AlertService.success("Ditt lösenord är uppdaterat!");
+
+        }, 1000);
+      }
+
+    }
+
+    function resetProfileForm() {
+      vm.user = angular.copy($rootScope.globals.currentUser);
+    }
+
+    function resetPasswordForm() {
+      vm.user.password = '';
+      vm.user.passwordRepeat = '';
     }
 
   }
+
 
 })();
