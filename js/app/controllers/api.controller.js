7777acbc41c4327f59f0437221cac3224919f6e7
Mikael Lindberg <mikael@lindbergit.se>
2015-10-21 11:40:50 +0200

    Added api controller, added model to view, adjust path in index.

--- js/app/controllers/api.controller.js
+++ js/app/controllers/api.controller.js
@@ -11,13 +11,28 @@
     var vm = this;
 
     APIService.call('apisApiIdGet', [$routeParams.apiName + '/' + $routeParams.apiVersion + '/' + $routeParams.apiProvider])
-    .then(aPIsIdGetResponse);
+    .then(aPIsIdGetResponse)
+    .then(getDocumentsForApi());
+
+    function getDocumentsForApi()
+    {
+      APIService.call('apisApiIdDocumentsGet',[0,0, $routeParams.apiName + '/' + $routeParams.apiVersion + '/' + $routeParams.apiProvider] ).
+      then(apisApiIdDocumentsGetResponse)
+    }
 
     function aPIsIdGetResponse(response) {
       if (response.status == 200) {
         vm.api = response.data;
       } else {
         AlertService.error("Problem retrieving api details");
+      }
+    }
+
+    function apisApiIdDocumentsGetResponse(response){
+      if (response.status == 200){
+        vm.api.documents = response.data.list;
+      } else {
+        AlertService.error("Could not retrieve document list for API");
       }
     }
   }
