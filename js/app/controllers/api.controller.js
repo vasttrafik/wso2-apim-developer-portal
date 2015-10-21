a5fd686e4d9b6e7fd59e33f06ff1f6b7d13cc9ad
Mikael Lindberg <mikael@lindbergit.se>
2015-10-21 15:34:52 +0200

    Adjustments to ApiCtrl.

--- js/app/controllers/api.controller.js
+++ js/app/controllers/api.controller.js
@@ -10,14 +10,18 @@
   function ApiCtrl($routeParams, $http, $httpParamSerializer, APIService, AlertService) {
     var vm = this;
 
+    vm.documents = {};
+    vm.applications = {};
+    vm.subscriptions = {};
+
     APIService.call('apisApiIdGet', [$routeParams.apiName + '/' + $routeParams.apiVersion + '/' + $routeParams.apiProvider])
-    .then(aPIsIdGetResponse)
-    .then(getDocumentsForApi());
+      .then(aPIsIdGetResponse)
+      .then(getDocumentsForApi);
 
     function getDocumentsForApi()
     {
-      APIService.call('apisApiIdDocumentsGet',[0,0, $routeParams.apiName + '/' + $routeParams.apiVersion + '/' + $routeParams.apiProvider] ).
-      then(apisApiIdDocumentsGetResponse)
+      APIService.call('apisApiIdDocumentsGet', [10, 10, $routeParams.apiName + '/' + $routeParams.apiVersion + '/' + $routeParams.apiProvider])
+        .then(apisApiIdDocumentsGetResponse)
     }
 
     function aPIsIdGetResponse(response) {
@@ -28,9 +32,9 @@
       }
     }
 
-    function apisApiIdDocumentsGetResponse(response){
-      if (response.status == 200){
-        vm.api.documents = response.data.list;
+    function apisApiIdDocumentsGetResponse(docResponse){
+      if (docResponse.status == 200) {
+        vm.documents = docResponse.data.list;
       } else {
         AlertService.error("Could not retrieve document list for API");
       }
