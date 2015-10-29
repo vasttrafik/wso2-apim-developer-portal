498d314acffe43e8c8011780b806e197039224d9
Mikael Lindberg <mikael@lindbergit.se>
2015-10-29 15:00:39 +0100

    Added scroll capability.

--- js/app/controllers/guides.controller.js
+++ js/app/controllers/guides.controller.js
@@ -5,11 +5,23 @@
     .module('vtPortal')
     .controller('GuidesCtrl', GuidesCtrl);
 
-  GuidesCtrl.$inject = ['$routeParams'];
+  GuidesCtrl.$inject = ['$routeParams', '$anchorScroll', '$location', '$scope'];
 
-  function GuidesCtrl($routeParams) {
+  function GuidesCtrl($routeParams, $anchorScroll, $location, $scope) {
     var vm = this;
-    vm.template = { name: $routeParams.guide + '.view.html', url: 'js/app/views/guides/' + $routeParams.guide + '.view.html'};
+
+    $anchorScroll.yOffset = 100;
+
+    vm.template = { name: $routeParams.guide + '.view.html', url: 'js/app/views/guides/' + $routeParams.guide + '.view.html' };
+    vm.scrollTo = scrollTo;
+    
+    function scrollTo(hash) {
+      if ($location.hash() !== hash) {
+        $location.hash(hash);
+      } else {
+        $anchorScroll();
+      }
+    }
   }
 
 })();
