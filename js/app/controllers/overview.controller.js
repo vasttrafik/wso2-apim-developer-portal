a51849cd5f3fb46f62b67bd3a9f86092c3c04810
Christian Morin <christian.morin@vasttrafik.se>
2015-10-29 16:30:19 +0100

    Fix for overview controller initiation.

--- js/app/controllers/overview.controller.js
+++ js/app/controllers/overview.controller.js
@@ -10,33 +10,28 @@
   function OverviewCtrl($http, APIService, AlertService) {
     var vm = this;
 
-    function GuidesCtrl($routeParams) {
-      var vm = this;
+    (function init() {
+      APIService.call('applicationsGet', [0.0, 0.0])
+        .then(applicationsGetResponse);
 
-      (function init() {
-        APIService.call('applicationsGet', [0.0, 0.0])
-          .then(applicationsGetResponse);
+      APIService.call('subscriptionsGet', [0.0, 0.0])
+        .then(subscriptionsGetResponse);
+    })();
 
-        APIService.call('subscriptionsGet', [0.0, 0.0])
-          .then(subscriptionsGetResponse);
-      })();
-
-      function subscriptionsGetResponse(response) {
-        if (response.status === 200) {
-          vm.subscriptions = response.data.list;
-        } else {
-          AlertService.error("Problem retrieving subscription list");
-        }
+    function subscriptionsGetResponse(response) {
+      if (response.status === 200) {
+        vm.subscriptions = response.data.list;
+      } else {
+        AlertService.error("Problem retrieving subscription list");
       }
+    }
 
-      function applicationsGetResponse(response) {
-        if (response.status === 200) {
-          vm.applications = response.data.list;
-        } else {
-          AlertService.error("Problem retrieving application list");
-        }
+    function applicationsGetResponse(response) {
+      if (response.status === 200) {
+        vm.applications = response.data.list;
+      } else {
+        AlertService.error("Problem retrieving application list");
       }
-
     }
 
   }
