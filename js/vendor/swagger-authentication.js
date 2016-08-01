dfac2f2574891e4f1778130eb80dd5dbfb955c26
Christian Morin <christian@fourmation.se>
2016-08-01 09:14:02 +0200

    Do not send jwt token in API console requests when logged in.

--- js/vendor/swagger-authentication.js
+++ js/vendor/swagger-authentication.js
@@ -11,6 +11,7 @@ angular
       var deferred = $q.defer();
       if ($rootScope.user.accessToken != null) {
         data.headers.Authorization = 'Bearer ' + $rootScope.user.accessToken;
+        data.headers['X-JWT-Assertion'] = undefined; // Do not send jwt token in request
       }
       deferred.resolve(true);
       return deferred.promise;
